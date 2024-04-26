8d740e089154ae8b5880fc63c9704f12d26efa88
anmolbhatia289 <53425990+anmolbhatia289@users.noreply.github.com>
2024-04-26 19:49:23 +0530

    Different kinds of rate limiters

--- RateLimiter/RateLimiter/LeakyBucketRateLimiter.cs
+++ RateLimiter/RateLimiter/LeakyBucketRateLimiter.cs
@@ -7,54 +7,52 @@ using System.Threading.Tasks;
 
 namespace RateLimiter
 {
-    public class TokenBucketRateLimiter
+    public class LeakyBucketRateLimiter
     {
         private object _lock = new object();
 
         /// <summary>
-        /// Maximum number of tokens the bucket can hold
+        /// Maximum amount of water(requests) the bucket can hold
         /// </summary>
         private int _capacity = 10;
 
         /// <summary>
-        /// The number of tokens currenty lin the bucket.
+        /// The amount of water(requests) currenty in the bucket.
         /// </summary>
         private int _tokens = 10;
 
         /// <summary>
-        /// Time unit gap between token refill in ticks
+        /// The rate(in ticks) at which 1 unit of water is flowing.
         /// </summary>
         private int _rate = 100000;
 
-        private long _lastRefillTime = DateTime.UtcNow.Ticks;
+        private long _lastLeakTimeStamp = DateTime.UtcNow.Ticks;
 
         public bool TryAcquire()
         {
             long currentTime = DateTime.UtcNow.Ticks;
-            if (currentTime - _lastRefillTime > _rate)
-            {
-                long tokensToAdd = (currentTime - _lastRefillTime) / _rate;
-                if (tokensToAdd > 0) 
-                {
-                    lock (_lock)
-                    {
-                        _lastRefillTime = DateTime.UtcNow.Ticks;
-                        _tokens = Math.Min(_tokens + (int)tokensToAdd, _capacity);
-                    }
-                }
-            }
+            int leakedAmount = (int)(currentTime - _lastLeakTimeStamp)/_rate;
 
-            if (_tokens > 0)
+
+            if (leakedAmount > 0)
             {
                 lock (_lock)
                 {
-                    // Because till the time I get lock, another 
-                    // thread might reduce the token count further.
-                    if (_tokens > 0)
+                    _tokens = Math.Max(0, _tokens - leakedAmount);
+                    _lastLeakTimeStamp = currentTime;
+                }
+            }
+
+            if (_tokens < _capacity)
+            {
+                lock (_lock) 
+                {
+                    if (_tokens < _capacity)
                     {
-                        _tokens--;
+                        _tokens += 1;
                     }
                 }
+
                 Console.WriteLine("Pass through successful");
                 return true;
             }
@@ -63,11 +61,12 @@ namespace RateLimiter
                 Console.WriteLine("Request blocked");
                 return false;
             }
+
         }
 
         public static void NotMain()
         {
-            var rateLimiter = new TokenBucketRateLimiter();
+            var rateLimiter = new LeakyBucketRateLimiter();
             var threads = new List<Thread>();
             for (int i = 0; i < 1000; i++)
             {
---
+++ RateLimiter/RateLimiter/SlidingWindow.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace RateLimiter
+{
+    public class SlidingWindow
+    {
+        ConcurrentQueue<long> queue = new ConcurrentQueue<long>();
+
+        long windowLength = 1000000;
+
+        int maxLength = 10;
+        public SlidingWindow() { }
+        public bool TryAcquire()
+        {
+            long currentTime = DateTime.UtcNow.Ticks;
+            while (queue.Count != 0 && queue.TryPeek(out long time) && currentTime > time + windowLength) 
+            {
+                queue.TryDequeue(out time);
+            }
+
+            if (queue.Count <= maxLength)
+            {
+                queue.Enqueue(currentTime);
+                Console.WriteLine("Pass through successful");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Request blocked");
+                return false;
+            }
+        }
+
+        public static void Main()
+        {
+            var obj = new SlidingWindow();
+            var list = new List<Thread>();
+            for (int i = 0; i < 1000; i++)
+            {
+                var thread = new Thread(() => obj.TryAcquire());
+                thread.Start();
+                list.Add(thread);
+            }
+
+            foreach (var thread in list)
+            {
+                thread.Join();
+            }
+        }
+    }
+
+    
+}
